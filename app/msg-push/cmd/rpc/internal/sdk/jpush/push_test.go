cdf4e23b8bb652903c45c752370b3361cc1ca857
mashouyue's m1max <mashouyue@toowow.cn>
2022-05-18 17:33:39 +0800

    ApnsProduction config

---
+++ app/msg-push/cmd/rpc/internal/sdk/jpush/push_test.go
@@ -0,0 +1,25 @@
+package push
+
+import (
+	"context"
+	"github.com/showurl/Zero-IM-Server/app/msg-push/cmd/rpc/internal/config"
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	push := &JPush{Config: config.Config{
+		Jpns: config.JpnsConf{
+			PushIntent:   `intent:#Intent;component=com.xxx.xxx/com.xxx.xxx.MainActivity;end`,
+			PushUrl:      `https://api.jpush.cn/v3/push`,
+			AppKey:       ``,
+			MasterSecret: ``,
+		},
+	}}
+	resp, err := push.Push(context.Background(), []string{
+		"xxx",
+	}, "代码-你根本没在沈阳", "代码-我是云南的")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(resp)
+}
