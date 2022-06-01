0b0683567325f5821487cc6bb13c319d55e38d6e
mashouyue's m1max <mashouyue@toowow.cn>
2022-06-01 10:16:08 +0800

    msg-gateway 自检程序

--- app/msg-gateway/cmd/wsrpc/msggateway.go
+++ app/msg-gateway/cmd/wsrpc/msggateway.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Path-IM/Path-IM-Server/app/msg-gateway/cmd/wsrpc/internal/wsconfig"
 	"github.com/Path-IM/Path-IM-Server/app/msg-gateway/cmd/wsrpc/internal/wssvc"
 	"github.com/Path-IM/Path-IM-Server/app/msg-gateway/cmd/wsrpc/pb"
+	"github.com/gorilla/websocket"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
@@ -37,6 +38,15 @@ func ws() {
 	handler.RegisterHandlers(server, ctx)
 
 	fmt.Printf("Starting server at %s:%d...\n", wsConfig.Host, wsConfig.Port)
+	go func() {
+		time.Sleep(time.Second * 3)
+		_, _, err := websocket.DefaultDialer.Dial(
+			fmt.Sprintf("ws://127.0.0.1:%d?token=test-token&sendID=-1&platformID=Linux", wsConfig.Port),
+			nil)
+		if err != nil {
+			panic("自检程序失败:" + err.Error())
+		}
+	}()
 	server.Start()
 }
 
